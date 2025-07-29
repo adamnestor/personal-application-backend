5b8b400cf68092ef04b8978d3d2b9815f3dd481c
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-07-29 16:50:01 -0400

    updated repositories with User entity

--- src/main/java/com/personalapplication/repository/ExpenseTemplateRepository.java
+++ src/main/java/com/personalapplication/repository/ExpenseTemplateRepository.java
@@ -3,9 +3,13 @@ package com.personalapplication.repository;
 import com.personalapplication.domain.ExpenseTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExpenseTemplateRepository extends JpaRepository<ExpenseTemplate, Long> {
-    List<ExpenseTemplate> findByActiveTrue();
+    List<ExpenseTemplate> findByUserIdAndActiveTrue(Long userId);
+    List<ExpenseTemplate> findByUserId(Long userId);
+    Optional<ExpenseTemplate> findByUserIdAndId(Long userId, Long templateId);
 }
--- src/main/java/com/personalapplication/repository/ScheduledIncomeRepository.java
+++ src/main/java/com/personalapplication/repository/ScheduledIncomeRepository.java
@@ -3,9 +3,12 @@ package com.personalapplication.repository;
 import com.personalapplication.domain.ScheduledIncome;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ScheduledIncomeRepository extends JpaRepository<ScheduledIncome, Long> {
-    List<ScheduledIncome> findByYearValueAndMonthValueOrderByScheduledDate(int yearValue, int monthValue);
+    List<ScheduledIncome> findByUserIdAndYearValueAndMonthValueOrderByScheduledDate(Long userId, int yearValue, int monthValue);
+    Optional<ScheduledIncome> findByUserIdAndId(Long userId, Long incomeId);
 }
