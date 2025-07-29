eb59a8a474c6f2b9418bfb9c6b60301ceedf9143
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-07-29 15:32:24 -0400

    repositories for finance section

---
+++ src/main/java/com/personalapplication/repository/ScheduledIncomeRepository.java
@@ -0,0 +1,11 @@
+package com.personalapplication.repository;
+
+import com.personalapplication.domain.ScheduledIncome;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ScheduledIncomeRepository extends JpaRepository<ScheduledIncome, Long> {
+    List<ScheduledIncome> findByYearAndMonthOrderByScheduledDate(int year, int month);
+}
